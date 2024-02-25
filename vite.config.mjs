4a63513228479cca2ac65189c2a4bd3239c30511
Maciej Penar <Maciej.Penar@vulcan.edu.pl>
2024-02-25 10:57:51 +0100

    deployment config

--- vite.config.mjs
+++ vite.config.mjs
@@ -45,6 +45,9 @@ export default defineConfig({
     }),
   ],
   define: { 'process.env': {} },
+  base: process.env.NODE_ENV === 'production'
+    ? '/GospelAccordingToSQLServer/'
+    : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
